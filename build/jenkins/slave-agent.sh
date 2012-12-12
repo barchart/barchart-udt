f2690c7ef9646a07c3d980aad01a3b5e08a30549
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2012-12-12 14:00:08 -0600

    a

--- build/jenkins/slave-agent.sh
+++ build/jenkins/slave-agent.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Copyright (C) 2009-2012 Barchart, Inc. <http://www.barchart.com/>
 #
@@ -6,12 +7,9 @@
 # http://www.opensource.org/licenses/bsd-license.php
 #
 
-#/bin/sh
-
-
 #-----------------------------------------------------------------------------
 #
-#	${mavenStamp}
+#	${projectBuildStamp}
 #
 #-----------------------------------------------------------------------------
 # TESTED: redhat(chkconfig)
@@ -30,44 +28,35 @@
 #
 ### BEGIN INIT INFO
 # Provides:          slave-agent
-# Required-Start:    $local_fs $remote_fs $syslog
-# Required-Stop:     $local_fs $remote_fs $syslog
-# Should-Start:      $network $named $time
-# Should-Stop:       $network $named $time
+# Required-Start:    $network $local_fs $remote_fs $syslog
+# Required-Stop:     $network $local_fs $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: slave-agent
 # Description:       slave-agent
+# Short-Description: slave-agent
 ### END INIT INFO
 #-----------------------------------------------------------------------------
 
 # use this instead of "echo"
 log(){
 # one argument to echo
-# 	echo "### slave-agent: $1"
-# 	echo "### slave-agent: $(date)" >> slave-agent.log
 	logger "### slave-agent: $1"
+ 	echo   "### slave-agent: $1"
 	echo   "### slave-agent: $1" >> slave-agent.log
 }
 
-# Resolve the true real path and name of this script without any sym links;
-#REAL_PATH="$(dirname  "$(readlink -f -n $0)")"
-#REAL_NAME="$(basename "$(readlink -f -n $0)")"
+# Get the real fully qualified path to this script
+SCRIPT=$(readlink -f -n $0)
 
-THIS_PATH=$(cd ${0%/*} && echo $PWD/${0##*/})
-
-REAL_PATH=$(dirname $THIS_PATH)
-REAL_NAME=$(basename $THIS_PATH)
+# Resolve the any sym links;
+REAL_PATH=$(dirname  $SCRIPT)
 
 log "REAL_PATH=$REAL_PATH"
 
+JAVA_PID_FILE="$REAL_PATH/java.pid"
 
 # jenkins working directory defined by script location
 cd "$REAL_PATH"
-
-
-# Get the real fully qualified path to this script
-SCRIPT="$REAL_PATH/$REAL_NAME"
 
 # wrapper service link:
 # 	debian, redhat
@@ -94,38 +83,9 @@ do_uninstall(){
         exit 1
     fi
 }
-#################################
-
-# ensure network is available
-
-do_ping(){
-	log "network ping test"
-	ping 8.8.8.8 -c 1 -i 0.2 -t 100 -W 1 && echo "YES" || echo "NO"
-}
-
-IS_ONLINE=do_ping
-MAX_CHECKS=30
-CHECKS=0
-
-while [ $IS_ONLINE == "NO" ]; do
-    IS_ONLINE=do_ping
-    CHECKS=$[ $CHECKS + 1 ]
-    if [ $CHECKS -gt $MAX_CHECKS ]; then
-        break
-    fi
-done
-
-if [ $IS_ONLINE == "NO" ]; then
-	log "network is not available"
-    exit 1
-fi
-
-
-#################################
-
 
 # correction for the usual "think different" in macosx launchd
-think(){
+do_think(){
 
     status
 
@@ -141,100 +101,103 @@ think(){
 
 }
 
+do_start(){
 
-start(){
+	do_status
 
-  status
-
-  log "START: Jenkins Slave STARTING..."
+	log "START: Jenkins Slave STARTING..."
 
 	URL="https://jenkins.barchart.com/jnlpJars/slave.jar"
 
-	cd "$REAL_PATH"	
+	while [ true ] ; do
+		wget --no-check-certificate "$URL" -O "slave.jar" && break 
+		log "download failure URL=$URL"
+		sleep 1
+	done
 
-	wget --no-check-certificate "$URL" -O "slave.jar"
+	log "download success URL=$URL"
 
 	JAVA_CMD="java -jar slave.jar -jnlpUrl file:slave-agent.jnlp -noCertificateCheck -slaveLog slave.log" 
 
 	$JAVA_CMD > jenkins.log 2>&1 &
 
-	JAVA_PID=$!
+	JAVA_PID="$!"
 
-  disown $JAVA_PID
+	echo $JAVA_PID > $JAVA_PID_FILE
 
-  log "START: Jenkins Slave STARTED; JAVA_PID=$JAVA_PID"
+	disown $JAVA_PID
 
-}
-
-stop(){
-
-  status
-
-  log "STOP: Jenkins Slave STOPPING..."
-
-  # TODO kill by pid
-
-  PROC_ID=$( ps -ef | grep jenkins | grep slave | grep -v grep | awk '{ print $2 }' )
-
-  kill $PROC_ID
-
-  log "STOP: Jenkins Slave STOPPED"
+	log "START: Jenkins Slave STARTED; JAVA_PID=$JAVA_PID"
 
 }
 
-status(){
-	# TODO status by pid
-  PROC_COUNT=$( ps aux | grep jenkins | grep slave | grep -v grep | wc -l )
-  if [ $PROC_COUNT -gt 0 ]; then
-    log "STATUS: Jenkins Slave is RUNNING"
-  else
-    log "STATUS: Jenkins Slave is STOPPED"
-  fi
+do_stop(){
+
+	do_status
+
+	log "STOP: Jenkins Slave STOPPING..."
+	
+	kill $(cat $JAVA_PID_FILE)
+	
+	rm -f $JAVA_PID_FILE
+	
+	log "STOP: Jenkins Slave STOPPED"
+
 }
 
-restart(){
-  stop
-  start
+do_status(){
+	
+	if [ -f "$JAVA_PID_FILE" ]; then
+		log "STATUS: Jenkins Slave is RUNNING"
+	else
+		log "STATUS: Jenkins Slave is STOPPED"
+	fi
+
 }
 
-# See how we were called.
+do_restart(){
+	do_stop
+	do_start
+}
+
+
+#
+#
+#
 case "$1" in
 
-install)
-  do_install
- ;;
-
-uninstall)
-  do_uninstall
- ;;
-
-start)
- start
- ;;
-
-stop)
- stop
- ;;
-
-status)
- status
- ;;
-
-restart)
- restart
- ;;
-
-think)
-    think
-    ;;
-
-*)
- log $"Usage: $0 {install|uninstall|start|stop|status|restart}"
- exit 1
+	install)
+		do_install
+		;;
+	
+	uninstall)
+		do_uninstall
+		;;
+	
+	start)
+		do_restart
+		;;
+	
+	stop)
+		do_stop
+		;;
+	
+	status)
+		do_status
+		;;
+	
+	restart)
+		do_restart
+		;;
+	
+	think)
+	    do_think
+	    ;;
+	
+	*)
+		log $"Usage: $0 {install|uninstall|start|stop|status|restart}"
+		exit 1
 
 esac
 
-log "done"
-
 exit 0
-
