4406f55fc69930f460ff7c1b72519899988cb61d
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2012-12-14 13:10:03 -0600

    Revert "maven site barchart-udt-core-2.0.1-SNAPSHOT"
    This reverts commit 925b226684202e51d6c2b87ae23d13234f3fd4f9.

--- site/barchart-udt-core/doxygen/navtree.js
+++ site/barchart-udt-core/doxygen/navtree.js
@@ -1,6 +1,6 @@
 var NAVTREE =
 [
-  [ "barchart-udt-core-2.0.1-SNAPSHOT", "index.html", [
+  [ "barchart-udt-core-2.0.0", "index.html", [
     [ "Classes", null, [
       [ "Class List", "annotated.html", "annotated" ],
       [ "Class Index", "classes.html", null ],
