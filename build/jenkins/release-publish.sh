1c55ced31f0c9d16fc7fefe2fe597daf9db271af
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-12-13 20:39:39 -0600

    license

--- build/jenkins/release-publish.sh
+++ build/jenkins/release-publish.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# Copyright (C) 2009-2012 Barchart, Inc. <http://www.barchart.com/>
+#
+# All rights reserved. Licensed under the OSI BSD License.
+#
+# http://www.opensource.org/licenses/bsd-license.php
+#
 
 echo "##################################"
 
