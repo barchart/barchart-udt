112ad3a9b4fb5c60e01e982a5189586e3e776864
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2012-12-20 11:01:42 -0600

    netty modules

---
+++ barchart-udt-netty4/src/test/java/com/barchart/udt/netty/TestDummy.java
@@ -0,0 +1,17 @@
+package com.barchart.udt.netty;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+public class TestDummy {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
