6d16688ec0a2d10315bc614366e205ad47aa6bb5
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-03-19 18:02:50 -0500

    a

--- barchart-udt-core/src/main/java/com/barchart/udt/nio/ChannelUDT.java
+++ barchart-udt-core/src/main/java/com/barchart/udt/nio/ChannelUDT.java
@@ -47,8 +47,9 @@ public interface ChannelUDT extends Channel {
 
 	/**
 	 * Mask of all interest options which are permitted for this channel.
+	 * <p>
+	 * See {@link SelectionKey}
 	 * 
-	 * @see {@link SelectionKey}
 	 */
 	int validOps();
 
