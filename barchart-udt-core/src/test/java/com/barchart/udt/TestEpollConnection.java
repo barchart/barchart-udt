89dde80a61eba8725d4527fd88712a36651b9e54
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-05-11 13:10:14 -0500

    connection review

---
+++ barchart-udt-core/src/test/java/com/barchart/udt/TestEpollConnection.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (C) 2009-2013 Barchart, Inc. <http://www.barchart.com/>
+ *
+ * All rights reserved. Licensed under the OSI BSD License.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+package com.barchart.udt;
+
+import static org.junit.Assert.*;
+import static util.UnitHelp.*;
+
+import java.nio.IntBuffer;
+
+import org.junit.Test;
+
+import util.TestAny;
+
+import com.barchart.udt.util.HelpUDT;
+
+public class TestEpollConnection extends TestAny {
+
+	static EpollUDT.Opt opt(final SocketUDT socket) throws ExceptionUDT {
+		final int code = socket.getOption(OptionUDT.Epoll_Event_Mask);
+		return EpollUDT.Opt.from(code);
+	}
+
+	/**
+	 */
+	@Test
+	public void connectionOnOff() throws Exception {
+
+		final IntBuffer readBuffer = HelpUDT.newDirectIntBufer(10);
+		final IntBuffer writeBuffer = HelpUDT.newDirectIntBufer(10);
+		final IntBuffer sizeBuffer = HelpUDT.newDirectIntBufer(10);
+
+		final int epollID = SocketUDT.epollCreate0();
+
+		final SocketUDT accept = new SocketUDT(TypeUDT.DATAGRAM);
+		accept.setBlocking(false);
+		accept.bind0(localSocketAddress());
+		accept.listen0(1);
+
+		socketAwait(accept, StatusUDT.LISTENING);
+		log.info("accept {} {}", opt(accept), accept);
+
+		final SocketUDT client = new SocketUDT(TypeUDT.DATAGRAM);
+		client.setBlocking(false);
+		client.bind0(localSocketAddress());
+
+		SocketUDT.epollAdd0(epollID, client.id(), EpollUDT.Opt.BOTH.code);
+
+		socketAwait(client, StatusUDT.OPENED);
+		log.info("client {} {}", opt(client), client);
+
+		client.connect(accept.getLocalSocketAddress());
+
+		socketAwait(client, StatusUDT.CONNECTED);
+		log.info("client {} {}", opt(client), client);
+
+		{
+			log.info("### 1 ###");
+			final int readyCount = SocketUDT.selectEpoll( //
+					epollID, readBuffer, writeBuffer, sizeBuffer, 0);
+			assertEquals("", 1, readyCount);
+			assertTrue(socketPresent(client, writeBuffer));
+			logBuffer("read ", readBuffer);
+			logBuffer("write", writeBuffer);
+			clear(readBuffer);
+			clear(writeBuffer);
+		}
+
+		final SocketUDT server = accept.accept();
+		assertNotNull(server);
+
+		SocketUDT.epollAdd0(epollID, server.id(), EpollUDT.Opt.BOTH.code);
+
+		log.info("server {} {}", opt(server), server);
+
+		{
+			log.info("### 2 ###");
+			final int readyCount = SocketUDT.selectEpoll( //
+					epollID, readBuffer, writeBuffer, sizeBuffer, 0);
+			assertEquals("", 2, readyCount);
+			assertTrue(socketPresent(client, writeBuffer));
+			assertTrue(socketPresent(server, writeBuffer));
+			logBuffer("read ", readBuffer);
+			logBuffer("write", writeBuffer);
+			clear(readBuffer);
+			clear(writeBuffer);
+		}
+
+		client.close();
+		socketAwait(client, StatusUDT.CLOSED);
+		socketAwait(server, StatusUDT.BROKEN);
+
+		log.info("client {}", client);
+		log.info("server {}", server);
+
+		{
+			log.info("### 3 ###");
+			final int readyCount = SocketUDT.selectEpoll( //
+					epollID, readBuffer, writeBuffer, sizeBuffer, 0);
+			assertEquals("", 1, readyCount);
+			assertTrue(socketPresent(server, writeBuffer));
+			logBuffer("read ", readBuffer);
+			logBuffer("write", writeBuffer);
+			clear(readBuffer);
+			clear(writeBuffer);
+		}
+
+		client.close();
+		server.close();
+		accept.close();
+
+		SocketUDT.epollRelease0(epollID);
+
+	}
+
+}
